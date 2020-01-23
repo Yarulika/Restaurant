8acabce49b906473323d578da4842de4d9ded617
Anna Yarulina <anna.yarulina@gmail.com>
2020-01-23 20:18:04 +0200

    draft tests (?)

--- src/test/java/com/sda/restaurant/service/PersonServiceTest.java
+++ src/test/java/com/sda/restaurant/service/PersonServiceTest.java
@@ -1,16 +1,27 @@
 package com.sda.restaurant.service;
 
+import com.sda.restaurant.dao.PersonRepository;
+import com.sda.restaurant.entity.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 class PersonServiceTest {
 
-    @Test
-    void ifPersonExistsByEmail() {
-
-
-
-
-    }
+//    @Autowired
+//    private PersonRepository personRepository;
+//
+//    @Test
+//    void basicTest() {
+//        given(personRepository.findById(1)).willReturn(Optional.empty());
+//
+//    PersonService personService = new PersonService();
+//    personService.findById(1);
+//
+//    }
 }
