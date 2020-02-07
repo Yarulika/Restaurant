8849012e2775ff489e223c94e39e1218261cd054
Anna Yarulina <anna.yarulina@gmail.com>
2020-02-07 14:06:02 +0200

    brushing up

--- src/main/java/com/sda/restaurant/RestaurantApplication.java
+++ src/main/java/com/sda/restaurant/RestaurantApplication.java
@@ -3,8 +3,6 @@ package com.sda.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//TODO: remove below line
-//@Configuration + @EnableAutoConfiguration + @ComponentScan
 @SpringBootApplication
 public class RestaurantApplication {
 
